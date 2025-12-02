28ff2e2bc2d16ea47b7cc2fc7da6df5244d26d7e
minori Koshikawa <greenboy08031115@gmail.com>
2025-12-02 15:54:12 +0900

    feat(request-list): 承認待ち・承認済みの申請一覧取得機能を実装

--- app/Http/Controllers/AttendanceCorrectionController.php
+++ app/Http/Controllers/AttendanceCorrectionController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AttendanceCorrection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AttendanceCorrectionController extends Controller
 {
     public function index(Request $request) {
         $headerType = 'user';
+        $userId = Auth::id();
+        // ログインユーザーの申請だけに絞る
+        $attendanceCorrectionQuery = AttendanceCorrection::whereHas('attendance', function($query) use ($userId) {
+            $query->where('user_id', $userId);
+        });
 
-        return view('request.request_list', compact('headerType'));
+        $page = $request->query('page', 'wait');
+
+        if ($page === 'done') {
+            $attendanceCorrectionQuery->where('status', true);
+        } else {
+            $attendanceCorrectionQuery->where('status', false);
+        }
+
+        // 申請データを降順で取得
+        $attendanceCorrections = $attendanceCorrectionQuery->orderByDesc('created_at')->get();
+
+        return view('request.request_list', compact('headerType', 'attendanceCorrections', 'page'));
     }
 }
--- resources/views/request/request_list.blade.php
+++ resources/views/request/request_list.blade.php
@@ -12,8 +12,8 @@
             <h1 class="request-list__title">申請一覧</h1>
 
             <div class="tabs">
-                <a href="{{ route('requests.index', ['page' => 'wait']) }}" class="tab {{ request('page', 'wait') === 'wait' ? 'tab--active' : '' }}">承認待ち</a>
-                <a href="{{ route('requests.index', ['page' => 'done']) }}" class="tab {{ request('page') === 'done' ? 'tab--active' : '' }}">承認済み</a>
+                <a href="{{ route('requests.index', ['page' => 'wait']) }}" class="tab {{ $page === 'wait' ? 'tab--active' : '' }}">承認待ち</a>
+                <a href="{{ route('requests.index', ['page' => 'done']) }}" class="tab {{ $page === 'done' ? 'tab--active' : '' }}">承認済み</a>
             </div>
 
             <table>
@@ -29,43 +29,37 @@
                 </thead>
 
                 <tbody class="request-list__body-row">
-                    {{--@foreach ($dates as $date)
-                        @php
-                            $dateCarbon = \Carbon\Carbon::parse($date);
-                            $attendanceForDate = $attendances->firstWhere('work_date', $dateCarbon);
-                            $formattedDate = $dateCarbon->isoFormat('MM/DD(ddd)');
-                        @endphp
+                    @foreach ($attendanceCorrections as $attendanceCorrection)
                         <tr>
-                            <td class="table-days">{{ $formattedDate }}</td>
-                            <td class="table-clock-in">
-                                {{ ($attendanceForDate && $attendanceForDate->clock_in_at)
-                                    ? $attendanceForDate->clock_in_at->format('H:i') : '' }}
+                            <td class="table-situation">
+                                {{ $attendanceCorrection->status ? '承認済み' : '承認待ち' }}
                             </td>
 
-                            <td class="table-clock-out">
-                                {{ ($attendanceForDate && $attendanceForDate->clock_out_at)
-                                    ? $attendanceForDate->clock_out_at->format('H:i') : '' }}
+                            <td class="table-name">
+                                {{ optional(optional($attendanceCorrection->attendance)->user)->name }}
                             </td>
 
-                            <td class="table-break-total">
-                                {{ ($attendanceForDate && $attendanceForDate->the_total_break)
-                                    ? $attendanceForDate->the_total_break : '' }}
+                            <td class="table-work-date">
+                                {{ optional(optional($attendanceCorrection->attendance)->work_date)->format('Y/m/d')}}
                             </td>
 
-                            <td class="table-attendance-total">
-                                {{ ($attendanceForDate && $attendanceForDate->the_total_work)
-                                    ? $attendanceForDate->the_total_work : '' }}
-                            </td> --}}
+                            <td class="table-request-reason">
+                                {{ $attendanceCorrection->requested_notes}}
+                            </td>
+
+                            <td class="table-request-at">
+                                {{ $attendanceCorrection->created_at->format('Y/m/d')}}
+                            </td>
 
                             <td class="table-detail">
                                 {{--@if ($attendanceForDate)
-                                    <a class="button-detail" href="{{ route('attendance.detail', ['attendance' => $attendanceForDate->id]) }}">詳細</a>
-                                @else--}}
+                                    <a class="button-detail" href="{{ route('requests.index', ['attendance' => $attendanceForDate->id]) }}">詳細</a>
+                                @else}--}}
                                     <button class="button-detail">詳細</button>
-                                {{--@endif---}}
+                                {{--@endif--}}
                             </td>
                         </tr>
-                    {{--@endforeach--}}
+                    @endforeach
                 </tbody>
             </table>
         </div>
